e2e52a080e0edf6c7f9d6e5b394a4f738ec8a781
Yutabass <yutabassbass@gmail.com>
2019-06-12 14:22:52 +0900

    'viewの空白修正'

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -8,32 +8,32 @@
       {{ csrf_field() }}
       <input class="form-control" name="user_id" type="hidden" value="{{ $dailyreport->user_id }}">
         <div class="form-group form-size-small  
-          @if ( $errors->has('reporting_time') )
+          @if ($errors->has('reporting_time'))
             {{ 'has-error' }}
           @endif
         "> 
           <input class="form-control" name="reporting_time" type="date" value="{{ old('reporting_time', $dailyreport->reporting_time) }}">
-          @if ( $errors->has('reporting_time') )
+          @if ($errors->has('reporting_time'))
             <span class="help-block">{{ $errors->first('reporting_time') }}</span>
           @endif
         </div>
         <div class="form-group 
-          @if ( $errors->has('title') )
+          @if ($errors->has('title'))
             {{ 'has-error' }}
           @endif
         ">
           <input class="form-control" placeholder="Title" name="title" type="text" value="{{ old('title', $dailyreport->title) }}">
-          @if ( $errors->has('title') )
+          @if ($errors->has('title'))
             <span class="help-block">{{ $errors->first('title') }}</span>
           @endif
         </div>
         <div class="form-group 
-          @if ( $errors->has('contents') )
+          @if ($errors->has('contents'))
             {{ 'has-error' }}
           @endif
         ">
           <textarea class="form-control" placeholder="Content" name="contents" cols="50" rows="10">{{ old('contents',$dailyreport->contents) }}</textarea>
-          @if ( $errors->has('contents') )
+          @if ($errors->has('contents'))
             <span class="help-block">{{ $errors->first('contents') }}</span>
           @endif
         </div>     
